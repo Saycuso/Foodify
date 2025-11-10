8588e70128cb3d62b163a280db1a5a219ce40672
Abhishek <abhikhairnar440@gmail.com>
2025-11-10 14:28:10 +0530

    finalizing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React, {lazy} from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import { createBrowserRouter, RouterProvider, Outlet} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet,  useLocation} from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import Cart from "./components/Cart";
 import RestaurantMenu from "./components/RestaurantMenu"; 
-import { useLocation } from "react-router-dom";
 import 'leaflet/dist/leaflet.css';
 import { RestaurantProvider } from "./components/RestaurantContext";
 
